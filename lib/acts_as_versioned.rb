32e8293a7f5cda7387178851260b396200fcd340
Félix Bellanger <felix.bellanger@gmail.com>
2013-04-08 16:18:53 +0200

    Small patch contd

--- lib/acts_as_versioned.rb
+++ lib/acts_as_versioned.rb
@@ -331,7 +331,7 @@ module ActiveRecord #:nodoc:
         # Clones a model.  Used when saving a new version or reverting a model's version.
         def clone_versioned_model(orig_model, new_model)
           self.class.versioned_columns.each do |col|
-            new_model[col.name] = orig_model.write_attribute(col.name) if orig_model.has_attribute?(col.name)
+            new_model[col.name] = orig_model.read_attribute(col.name) if orig_model.has_attribute?(col.name)
           end
 
           clone_inheritance_column(orig_model, new_model)
